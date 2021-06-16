9916ec95b76d4a2fabb538343ee432b5a9f35db0
Aleksey Belov <aleksey.belov.95@gmail.com>
2021-06-16 21:33:29 +0800

    token

--- src/services/organizations.ts
+++ src/services/organizations.ts
@@ -13,11 +13,9 @@ const organizationsService = {
   update: (body, id: string) => makeRequest(`/api/v2/admin/organizations/${id}`, "PUT", body),
   partnersList: () => makeRequest("/api/v2/admin/partners/find", "GET"),
   uploadImage: (image: any, onSuccess) => {
+    const token = localStorage.getItem("vzletaemAdminToken");
     var myHeaders = new Headers();
-    myHeaders.append(
-      "Authorization",
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImIyZDlhYTMyLTZhOWEtNDExMi05NDI0LTU4ZTY1ZTY5YTZkNiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiIrNzAwMDAwMDAwMDEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiIrNzAwMDAwMDAwMDEiLCJBc3BOZXQuSWRlbnRpdHkuU2VjdXJpdHlTdGFtcCI6IkxVMjUzU1QyWVI2RFhNR1JKNkxSSjVQMjJCM1dUSUU0IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoi0JDQtNC80LjQvSIsIm5iZiI6MTYyMTkxMjk4OCwiZXhwIjoxNjUzNDQ4OTg4LCJpc3MiOiJ0ZXN0LWNob3BwZXIucG9sb25pdW1hcnRzLmNvbSIsImF1ZCI6InRlc3QtY2hvcHBlci5wb2xvbml1bWFydHMuY29tIn0.nDt3yw96ZcdIiBBT4RQR0tciKrzSOwuFIuErNZ4F0nc"
-    );
+    myHeaders.append("Authorization", `Bearer ${token}`);
 
     var formdata = new FormData();
     formdata.append("Media", image, image.name);
