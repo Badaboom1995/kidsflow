7554874af636db5a43dd0f78509fac40e6d246bc
Badaboom1995 <aleksey.belov.95@gmail.com>
2022-01-24 21:55:25 +0300

    conf var

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
-import { GlobalStyle } from "../global-styles";
-import Router from "./router";
-import Layout from "features/Layout";
-import { Switch, Route, useHistory } from "react-router-dom";
-import Auth from "pages/Auth";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useSelector } from "react-redux";
-import { selectUserData } from "ducks/user/selectors";
+import React, { useEffect } from 'react';
+import { GlobalStyle } from '../global-styles';
+import Router from './router';
+import Layout from 'features/Layout';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Auth from 'pages/Auth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
+import { selectUserData } from 'ducks/user/selectors';
 
 // TODO. make router right way
 function App() {
+  console.log(process.env, process.env.REACT_APP_SHOW_DEV);
   const history = useHistory();
   const user = useSelector(selectUserData);
-  const isLoggedIn = localStorage.getItem("vzletaemAdminToken");
+  const isLoggedIn = localStorage.getItem('vzletaemAdminToken');
   const Private = (
     <Layout>
       <Router />
